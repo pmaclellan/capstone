ea569386004850716c9bdfe6b38bc69188b93c8a
pmaclellan <maclellan.pete@gmail.com>
2016-09-20 09:21:29 -0400

    update plot with incoming data

--- gui.py
+++ gui.py
@@ -8,7 +8,9 @@ import Queue
 import matplotlib
 matplotlib.use('TkAgg')
 from matplotlib import pyplot as plt
+import matplotlib.animation as animation
 
+import numpy as np
 from numpy import arange, sin, pi
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2TkAgg
 # implement the default mpl key bindings
@@ -60,19 +62,39 @@ class DaqConnection:
 
 root = Tk.Tk()
 root.wm_title("DAQula Mock Data")
-
+daq = DaqConnection()
 # Create matplotlib Figure
 # this contains the graph of incoming data
-fig = Figure(figsize=(5, 4), dpi=100)
-axes = fig.add_subplot(111)
-timesteps = arange(0.0, 3.0, 0.01)
-s = 0.7*sin(2*pi*timesteps)
+# fig = Figure(figsize=(5, 4), dpi=100)
+# axes = fig.add_subplot(111)
+# timesteps = arange(0.0, 3.0, 0.01)
+# s = 0.7*sin(2*pi*timesteps)
 
-axes.plot(timesteps, s)
+# axes.plot(timesteps, s)
 
+def update_line(num, data, line):
+  if not daq.queue.empty():
+    new_value = daq.queue.get()
+    data.append(new_value)
+    if len(data) > 255:
+      data = data[1:]
+    line.set_data(xrange(len(data)), data)
+  return line,
+
+fig1 = plt.figure()
+
+data = []
+l, = plt.plot([], [], 'r-')
+plt.xlim(0, 256)
+plt.ylim(0, 256)
+plt.xlabel('x')
+plt.title('DAQula')
+line_ani = animation.FuncAnimation(fig1, update_line, 500, fargs=(data, l),
+                                   interval=50, blit=True)
+plt.show()
 
 # a tk.DrawingArea
-canvas = FigureCanvasTkAgg(fig, master=root)
+canvas = FigureCanvasTkAgg(fig1, master=root)
 canvas.show()
 canvas.get_tk_widget().pack(side=Tk.TOP, fill=Tk.BOTH, expand=1)
 
@@ -92,8 +114,6 @@ def _quit():
   root.quit()     # stops mainloop
   root.destroy()  # this is necessary on Windows to prevent
                   # Fatal Python Error: PyEval_RestoreThread: NULL tstate
-
-daq = DaqConnection()
 
 # connectButton = Tk.Button(master=root, text='Connect', command=daq._connect_to_server)
 # connectButton.pack(side=Tk.BOTTOM)
