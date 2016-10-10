d0d85413da2ca8226072739fa288e0d2a4a03c53
Dominic Harkness <dominic.harkness@gmail.com>
2016-10-10 17:16:38 -0400

    Saving changes. Not working. Seg fault when getting value from memory (line 85)

--- dma_driver/reader.c
+++ dma_driver/reader.c
@@ -3,18 +3,35 @@
 #include <sys/stat.h>
 #include <unistd.h>
 
+#define REGISTER_COUNT 8
+
 int main()
 {
-    int fd;
-    char * myfifo = "./myfifo";
+    // Define the file paths to write to
+    char* dmaFifoPaths[] = {
+        "/tmp/dma-register-1",
+        "/tmp/dma-register-2",
+        "/tmp/dma-register-3",
+        "/tmp/dma-register-4",
+        "/tmp/dma-register-5",
+        "/tmp/dma-register-6",
+        "/tmp/dma-register-7",
+        "/tmp/dma-register-8"
+    };
     int buf;
+    int fd;
+    int i;
 
     /* open, read, and display the message from the FIFO */
     while(1) {
-        fd = open(myfifo, O_RDONLY);
-        read(fd, &buf, sizeof(buf));
-        printf("Received: %d\n", buf);
-        close(fd);
+        for(i = 0; i < REGISTER_COUNT; i++)
+        {
+            fd = open(dmaFifoPaths[i], O_RDONLY);
+            read(fd, &buf, sizeof(buf));
+            printf("Received from register %d: %d\n", i, buf);
+            close(fd); 
+        }
+
     }
 
     return 0;
--- dma_driver/writer.c
+++ dma_driver/writer.c
@@ -1,34 +1,45 @@
 #include <stdio.h>
+#include <stdint.h>
 #include <stdlib.h>
 #include <fcntl.h>
 #include <sys/mman.h>
 #include <unistd.h>
 
 #define REGISTER_COUNT 8
-#define BASE_REGISTER 100 // 100 for test
+#define VALUES_SIZE 34
+#define MESSAGE_HEADER 0xDEAD
+#define MASK_ADC0 0xFFFF000000000000
+#define MASK_ADC1 0x0000FFFF00000000
+#define MASK_ADC2 0x00000000FFFF0000
+#define MASK_ADC3 0x000000000000FFFF
 
 int main (int argc, char* argv[])
 {
     // Define the file paths to write to
-    char* dmaFifoPaths[] = {
-        "/tmp/dma-register-1",
-        "/tmp/dma-register-2",
-        "/tmp/dma-register-3",
-        "/tmp/dma-register-4",
-        "/tmp/dma-register-5",
-        "/tmp/dma-register-6",
-        "/tmp/dma-register-7",
-        "/tmp/dma-register-8"
+    char* dmaFifoPath = "/tmp/dma-fifo";
+
+    // Define registers to read from
+    unsigned registers[] = {
+        0xb7758000,
+        0xb7758000,
+        0xb7758000,
+        0xb7758000,
+        0xb7758000,
+        0xb7758000,
+        0xb7758000,
+        0xb7758000
     };
 
-    int fd, fifoFd;
-    unsigned gpio_addr;
-    int value = 0;
-    char * myfifo = "./myfifo";
+    uint64_t value;
+    uint16_t values[VALUES_SIZE]; // 32 channels plus timestamp and header
     
-    unsigned page_addr, page_offset;
-    void *ptr;
-    unsigned page_size=sysconf(_SC_PAGESIZE);
+    int fd;
+    int fifoFd;
+    int i;
+    unsigned gpio_addr;
+    unsigned page_addr;
+    unsigned page_offsets[REGISTER_COUNT];
+    unsigned page_size=sizeof(uint64_t);
 
     /* Open /dev/mem file */
     fd = open("/dev/mem", O_RDWR);
@@ -37,42 +48,77 @@ int main (int argc, char* argv[])
         return -1;
     }
 
-    // strtoul take are startaddr and returns a pointer
-    // TODO: This might not be necessary, but idk what it does
-    gpio_addr=READ_ADDR;
-
-    /* mmap the device into memory */
-    page_addr = (gpio_addr & (~(page_size-1)));
-    page_offset = gpio_addr - page_addr;
-    ptr = mmap(NULL, page_size, PROT_READ|PROT_WRITE, MAP_SHARED, fd, page_addr);
-
-
     // Make a bunch of fifos and initialize mmap
-    for(int i = 0; i < REGISTER_COUNT; i++)
+    void* ptrs[REGISTER_COUNT];
+    for(i = 0; i < REGISTER_COUNT; i++)
     {
-        mkfifo(dmaFifoPaths[i], 0666);
-        gpio_addr = BASE_REGISTER;
+        mkfifo(dmaFifoPath, 0666);
+        gpio_addr = registers[i];
         page_addr = (gpio_addr & (~(page_size-1)));
-        page_offset = gpio_addr - page_addr;
-        ptr = mmap(NULL, page_size, PROT_READ|PROT_WRITE, MAP_SHARED, fd, page_addr);
+        page_offsets[i] = gpio_addr - page_addr;
+        ptrs[i] = mmap(NULL, page_size, PROT_READ|PROT_WRITE, MAP_SHARED, fd, page_addr);
     }
 
     while(1)
     {
-        /* Read value from the device register */
-        value = *((unsigned *)(ptr + page_offset));
-        printf("Data here is: %08x\n",value);
+        // prepare the packet with a header and timestamp
+        values[0] = MESSAGE_HEADER;
+        values[1] = 0x0000; // TODO: this should be the timestamp
+        // When reading the register, you get 4 channel values:
+        // REGISTER 1: 0.0  1.0  2.0  3.0
+        // REGISTER 2: 0.1  1.1  2.1  3.1
+        // ....
+        // REGISTER 3: 0.7  1.7  2.7  3.7
+        // We want to transmit in the following format:
+        // HEADER, TIMESTAMP, 0.0, 0.1, ... , 3.6, 3.7
+        // First set the offsets for the ADC's
+        int adc0 = 2;   // index 2 corresponds to the 0.0 spot
+        int adc1 = 10;  // index 10 corresponds to the 1.0 spot
+        int adc2 = 18;  // index 18 corresponds to the 2.0 spot
+        int adc3 = 26;  // index 26 corresponds to the 3.0 spot
+        int registerNumber = 0;
 
-        // Write data to fifo
-        fifoFd = open(myfifo, O_WRONLY);
-        write(fifoFd, & value, sizeof(value));
+        // Read registers and put our data into values
+        while(registerNumber <  REGISTER_COUNT)
+        {
+            // Read the register
+            value = *((uint64_t *)(ptrs[i] + page_offsets[i]));
+
+            // Get adc values
+            values[adc0] = (uint16_t)((MASK_ADC0 & value) >> 48);
+            values[adc1] = (uint16_t)((MASK_ADC1 & value) >> 32);
+            values[adc2] = (uint16_t)((MASK_ADC2 & value) >> 16);
+            values[adc3] = (uint16_t)(MASK_ADC3 & value);
+
+            // Write data to fifo
+            write(fifoFd, &value, sizeof(values));
+            close(fifoFd);
+
+            // Increment counters
+            adc0++;
+            adc1++;
+            adc2++;
+            adc3++;
+            registerNumber++;
+        }
+
+        // Write to the fifo
+        i = 0;
+        fifoFd = open(dmaFifoPath, O_WRONLY);
+        while(i < VALUES_SIZE)
+        {
+            write(fifoFd, &values[i], sizeof(uint16_t));
+        }
         close(fifoFd);
 
-        /* Put data into a pipe */
+
         sleep(1);
     }
 
-    munmap(ptr, page_size);
-    return 0;
+    for (i = 0; i < REGISTER_COUNT; i++)
+    {
+        munmap(ptrs[i], page_size);
+    }
 
+    return 0;
 }
