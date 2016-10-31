0bea0c381aa752d8cd5a2216124cc7ab51d41212
pmaclellan <maclellan.pete@gmail.com>
2016-10-31 17:33:35 -0400

    added type checking

--- RemoteApplication/network_controller.py
+++ RemoteApplication/network_controller.py
@@ -1,19 +1,24 @@
 import socket
-from multiprocessing import queues
+import multiprocessing
 
 class NetworkController():
     def __init__(self, storage_queue, gui_queue):
         print 'NetworkController __init__()'
 
-        if isinstance(storage_queue, queues.SimpleQueue):
+        if type(storage_queue) is multiprocessing.queues.Queue:
             self.storage_queue = storage_queue
         else:
-            raise TypeError('storage_queue must be a SimpleQueue')
+            raise TypeError('arg 2 must be a multiprocessing.Queue, found \n%s' % str(type(storage_queue)))
 
-        if isinstance(gui_queue, queues.SimpleQueue):
+        if type(gui_queue) is multiprocessing.queues.Queue:
             self.gui_queue = gui_queue
         else:
-            raise TypeError('gui_queue must be a SimpleQueue')
+            raise TypeError('arg 2 must be a multiprocessing.Queue, found \n%s' % str(type(gui_queue)))
 
+        # create TCP sockets for communication with DaQuLa
         self.control_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.data_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+        # sent_dict will hold request messages that have been sent over the
+        # control_socket but have not yet been ACKed
+        self.sent_dict = {}
--- RemoteApplication/test_network_controller.py
+++ RemoteApplication/test_network_controller.py
@@ -1,9 +1,9 @@
 from network_controller import NetworkController
-from multiprocessing import queues
+import multiprocessing
 
 def test_creation():
-    stg_queue = queues.SimpleQueue()
-    gui_queue = queues.SimpleQueue()
+    stg_queue = multiprocessing.Queue()
+    gui_queue = multiprocessing.Queue()
     nc = NetworkController(stg_queue, gui_queue)
     assert(nc.control_sock is not None)
     assert(nc.data_sock is not None)
