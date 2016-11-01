ed891aaad53ab9c3c041e0351fdfa324a0f7ab9d
pmaclellan <maclellan.pete@gmail.com>
2016-11-01 15:28:24 -0400

    newly compiled protobuf messages

--- RemoteApplication/control_signals_pb2.py
+++ RemoteApplication/control_signals_pb2.py
@@ -19,7 +19,7 @@ DESCRIPTOR = _descriptor.FileDescriptor(
   name='control_signals.proto',
   package='',
   syntax='proto3',
-  serialized_pb=_b('\n\x15\x63ontrol_signals.proto\".\n\x0cStartRequest\x12\x0c\n\x04port\x18\x01 \x01(\r\x12\x10\n\x08\x63hannels\x18\x02 \x01(\r\"-\n\x0bStopRequest\x12\x0c\n\x04port\x18\x01 \x01(\r\x12\x10\n\x08\x63hannels\x18\x02 \x01(\r\"!\n\x11SampleRateRequest\x12\x0c\n\x04rate\x18\x01 \x01(\r\"j\n\x12SensitivityRequest\x12,\n\x07voltage\x18\x01 \x01(\x0e\x32\x1b.SensitivityRequest.Voltage\"&\n\x07Voltage\x12\x08\n\x04\x46IVE\x10\x00\x12\x07\n\x03TEN\x10\x01\x12\x08\n\x04VREF\x10\x02\x62\x06proto3')
+  serialized_pb=_b('\n\x15\x63ontrol_signals.proto\".\n\x0cStartRequest\x12\x0c\n\x04port\x18\x01 \x01(\r\x12\x10\n\x08\x63hannels\x18\x02 \x01(\r\"-\n\x0bStopRequest\x12\x0c\n\x04port\x18\x01 \x01(\r\x12\x10\n\x08\x63hannels\x18\x02 \x01(\r\"!\n\x11SampleRateRequest\x12\x0c\n\x04rate\x18\x01 \x01(\r\"j\n\x12SensitivityRequest\x12,\n\x07voltage\x18\x01 \x01(\x0e\x32\x1b.SensitivityRequest.Voltage\"&\n\x07Voltage\x12\x08\n\x04\x46IVE\x10\x00\x12\x07\n\x03TEN\x10\x01\x12\x08\n\x04VREF\x10\x02\"\xb0\x01\n\x0eRequestWrapper\x12\x10\n\x08sequence\x18\x01 \x01(\r\x12\x1e\n\x05start\x18\x02 \x01(\x0b\x32\r.StartRequestH\x00\x12\x1c\n\x04stop\x18\x03 \x01(\x0b\x32\x0c.StopRequestH\x00\x12\"\n\x04rate\x18\x04 \x01(\x0b\x32\x12.SampleRateRequestH\x00\x12#\n\x04sens\x18\x05 \x01(\x0b\x32\x13.SensitivityRequestH\x00\x42\x05\n\x03msgb\x06proto3')
 )
 _sym_db.RegisterFileDescriptor(DESCRIPTOR)
 
@@ -190,12 +190,91 @@ _SENSITIVITYREQUEST = _descriptor.Descriptor(
   serialized_end=261,
 )
 
+
+_REQUESTWRAPPER = _descriptor.Descriptor(
+  name='RequestWrapper',
+  full_name='RequestWrapper',
+  filename=None,
+  file=DESCRIPTOR,
+  containing_type=None,
+  fields=[
+    _descriptor.FieldDescriptor(
+      name='sequence', full_name='RequestWrapper.sequence', index=0,
+      number=1, type=13, cpp_type=3, label=1,
+      has_default_value=False, default_value=0,
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      options=None),
+    _descriptor.FieldDescriptor(
+      name='start', full_name='RequestWrapper.start', index=1,
+      number=2, type=11, cpp_type=10, label=1,
+      has_default_value=False, default_value=None,
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      options=None),
+    _descriptor.FieldDescriptor(
+      name='stop', full_name='RequestWrapper.stop', index=2,
+      number=3, type=11, cpp_type=10, label=1,
+      has_default_value=False, default_value=None,
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      options=None),
+    _descriptor.FieldDescriptor(
+      name='rate', full_name='RequestWrapper.rate', index=3,
+      number=4, type=11, cpp_type=10, label=1,
+      has_default_value=False, default_value=None,
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      options=None),
+    _descriptor.FieldDescriptor(
+      name='sens', full_name='RequestWrapper.sens', index=4,
+      number=5, type=11, cpp_type=10, label=1,
+      has_default_value=False, default_value=None,
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      options=None),
+  ],
+  extensions=[
+  ],
+  nested_types=[],
+  enum_types=[
+  ],
+  options=None,
+  is_extendable=False,
+  syntax='proto3',
+  extension_ranges=[],
+  oneofs=[
+    _descriptor.OneofDescriptor(
+      name='msg', full_name='RequestWrapper.msg',
+      index=0, containing_type=None, fields=[]),
+  ],
+  serialized_start=264,
+  serialized_end=440,
+)
+
 _SENSITIVITYREQUEST.fields_by_name['voltage'].enum_type = _SENSITIVITYREQUEST_VOLTAGE
 _SENSITIVITYREQUEST_VOLTAGE.containing_type = _SENSITIVITYREQUEST
+_REQUESTWRAPPER.fields_by_name['start'].message_type = _STARTREQUEST
+_REQUESTWRAPPER.fields_by_name['stop'].message_type = _STOPREQUEST
+_REQUESTWRAPPER.fields_by_name['rate'].message_type = _SAMPLERATEREQUEST
+_REQUESTWRAPPER.fields_by_name['sens'].message_type = _SENSITIVITYREQUEST
+_REQUESTWRAPPER.oneofs_by_name['msg'].fields.append(
+  _REQUESTWRAPPER.fields_by_name['start'])
+_REQUESTWRAPPER.fields_by_name['start'].containing_oneof = _REQUESTWRAPPER.oneofs_by_name['msg']
+_REQUESTWRAPPER.oneofs_by_name['msg'].fields.append(
+  _REQUESTWRAPPER.fields_by_name['stop'])
+_REQUESTWRAPPER.fields_by_name['stop'].containing_oneof = _REQUESTWRAPPER.oneofs_by_name['msg']
+_REQUESTWRAPPER.oneofs_by_name['msg'].fields.append(
+  _REQUESTWRAPPER.fields_by_name['rate'])
+_REQUESTWRAPPER.fields_by_name['rate'].containing_oneof = _REQUESTWRAPPER.oneofs_by_name['msg']
+_REQUESTWRAPPER.oneofs_by_name['msg'].fields.append(
+  _REQUESTWRAPPER.fields_by_name['sens'])
+_REQUESTWRAPPER.fields_by_name['sens'].containing_oneof = _REQUESTWRAPPER.oneofs_by_name['msg']
 DESCRIPTOR.message_types_by_name['StartRequest'] = _STARTREQUEST
 DESCRIPTOR.message_types_by_name['StopRequest'] = _STOPREQUEST
 DESCRIPTOR.message_types_by_name['SampleRateRequest'] = _SAMPLERATEREQUEST
 DESCRIPTOR.message_types_by_name['SensitivityRequest'] = _SENSITIVITYREQUEST
+DESCRIPTOR.message_types_by_name['RequestWrapper'] = _REQUESTWRAPPER
 
 StartRequest = _reflection.GeneratedProtocolMessageType('StartRequest', (_message.Message,), dict(
   DESCRIPTOR = _STARTREQUEST,
@@ -224,6 +303,13 @@ SensitivityRequest = _reflection.GeneratedProtocolMessageType('SensitivityReques
   # @@protoc_insertion_point(class_scope:SensitivityRequest)
   ))
 _sym_db.RegisterMessage(SensitivityRequest)
+
+RequestWrapper = _reflection.GeneratedProtocolMessageType('RequestWrapper', (_message.Message,), dict(
+  DESCRIPTOR = _REQUESTWRAPPER,
+  __module__ = 'control_signals_pb2'
+  # @@protoc_insertion_point(class_scope:RequestWrapper)
+  ))
+_sym_db.RegisterMessage(RequestWrapper)
 
 
 # @@protoc_insertion_point(module_scope)
