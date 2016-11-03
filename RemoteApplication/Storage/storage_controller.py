2445cd09864096234bddfa205b0ba7710edf42db
pmaclellan <maclellan.pete@gmail.com>
2016-11-03 11:22:59 -0400

    convert to numpy arrays for writing to HDF5

--- RemoteApplication/Storage/storage_controller.py
+++ RemoteApplication/Storage/storage_controller.py
@@ -1,5 +1,6 @@
 import threading
 import h5py
+import numpy as np
 import time
 import Queue
 import os.path
@@ -17,23 +18,27 @@ class StorageController():
 
     def writefiles(self):
         timeout_threshold = 1000
+        filesize_threshold = 100000
         write_buffer = []
+        storage_type = np.dtype([('channel', 'S3'), ('value', 'i2')])
 
         while True:
             filename = time.strftime('%Y%m%d-%H:%M:%S')+'.h5'
-            f = h5py.File(filename, 'x')
+            f = h5py.File(filename, 'a')
+            print 'new file created: %s' % filename
             timeout_counter = 0
             i = 0
             while timeout_counter < timeout_threshold:
                 if not self.write_buffer.empty():
                     timeout_counter = 0
                     reading = self.write_buffer.get_nowait()
-                    write_buffer.append(reading)
+                    write_buffer.append(reading.astype(storage_type))
                     if len(write_buffer) > 200:
                         f.create_dataset(str(i), data=write_buffer)
+                        print 'wrote dataset %d' % i
                         write_buffer = []
                         i += 1
-                    if os.path.getsize(filename) > 1000:
+                    if os.path.getsize(filename) > filesize_threshold:
                         print 'file size %d exceeds limit, closing and starting new file' \
                               % os.path.getsize(filename)
                         f.close()
@@ -41,6 +46,11 @@ class StorageController():
                 else:
                     timeout_counter += 1
             if timeout_counter >= timeout_threshold:
+                if len(write_buffer) > 0:
+                    print 'buffer length to flush: %d' % len(write_buffer)
+                    print 'i = %d' % i
+                    #write the partial buffer to file before closing
+                    f.create_dataset(str(i), data=write_buffer)
                 f.close()
                 # if we timeout, stop writing files
                 break
--- RemoteApplication/Storage/test_storage_controller.py
+++ RemoteApplication/Storage/test_storage_controller.py
@@ -14,16 +14,15 @@ def test_grabbuffer_single():
     ingoing_buffer = mp.Queue()
     sc = StorageController(ingoing_buffer)
     expected_filename = time.strftime('%Y%m%d-%H:%M:%S') + '.h5'
-    reading = {('TS' , 15),
-               ('0.0', 0xDEADBEEF),
-               ('0.1', 0xBEADBEEF),
-               ('0.2', 0xBEADDEEF),
-               ('0.3', 0xDEAFBEEF),
-               ('1.0', 0xDEADBED0),
-               ('1.1', 0xEDBE98EF),
-               ('1.2', 0xDEA234EF),
-               ('1.3', 0xCA56BEEF),
-               }
+    reading = np.array([('0.0', 0xDEADBEEF),
+                        ('0.1', 0xDEADBEEF),
+                        ('0.2', 0xDEADBEEF),
+                        ('0.3', 0xDEADBEEF),
+                        ('1.0', 0xDEADBEEF),
+                        ('1.1', 0xDEADBEEF),
+                        ('1.2', 0xDEADBEEF),
+                        ('1.3', 0xDEADBEEF)],
+                       dtype=[('channel', 'S3'), ('value', 'i2')])
     ingoing_buffer.put(reading)
     time.sleep(0.1)
     assert ingoing_buffer.empty()
@@ -38,16 +37,15 @@ def test_grabbuffer_short_burst():
     sc = StorageController(ingoing_buffer)
     expected_filename = time.strftime('%Y%m%d-%H:%M:%S') + '.h5'
     for i in range(1000):
-        reading = {('TS' , random.random() * 15),
-                   ('0.0', random.randrange(0, 0xffffffff)),
-                   ('0.1', random.randrange(0, 0xffffffff)),
-                   ('0.2', random.randrange(0, 0xffffffff)),
-                   ('0.3', random.randrange(0, 0xffffffff)),
-                   ('1.0', random.randrange(0, 0xffffffff)),
-                   ('1.1', random.randrange(0, 0xffffffff)),
-                   ('1.2', random.randrange(0, 0xffffffff)),
-                   ('1.3', random.randrange(0, 0xffffffff)),
-                   }
+        reading = np.array([('0.0', random.randrange(0, 0xffffffff)),
+                            ('0.1', random.randrange(0, 0xffffffff)),
+                            ('0.2', random.randrange(0, 0xffffffff)),
+                            ('0.3', random.randrange(0, 0xffffffff)),
+                            ('1.0', random.randrange(0, 0xffffffff)),
+                            ('1.1', random.randrange(0, 0xffffffff)),
+                            ('1.2', random.randrange(0, 0xffffffff)),
+                            ('1.3', random.randrange(0, 0xffffffff))],
+                           dtype=[('channel', 'S3'), ('value', 'i2')])
         ingoing_buffer.put(reading)
     time.sleep(0.1)
     assert ingoing_buffer.empty()
@@ -60,16 +58,15 @@ def test_writefiles_file_created():
     sc = StorageController(ingoing_buffer)
     expected_filename = time.strftime('%Y%m%d-%H:%M:%S') + '.h5'
 
-    reading = {('TS', random.random() * 15),
-               ('0.0', random.randrange(0, 0xffffffff)),
-               ('0.1', random.randrange(0, 0xffffffff)),
-               ('0.2', random.randrange(0, 0xffffffff)),
-               ('0.3', random.randrange(0, 0xffffffff)),
-               ('1.0', random.randrange(0, 0xffffffff)),
-               ('1.1', random.randrange(0, 0xffffffff)),
-               ('1.2', random.randrange(0, 0xffffffff)),
-               ('1.3', random.randrange(0, 0xffffffff)),
-               }
+    reading = np.array([('0.0', random.randrange(0, 0xffffffff)),
+                        ('0.1', random.randrange(0, 0xffffffff)),
+                        ('0.2', random.randrange(0, 0xffffffff)),
+                        ('0.3', random.randrange(0, 0xffffffff)),
+                        ('1.0', random.randrange(0, 0xffffffff)),
+                        ('1.1', random.randrange(0, 0xffffffff)),
+                        ('1.2', random.randrange(0, 0xffffffff)),
+                        ('1.3', random.randrange(0, 0xffffffff))],
+                       dtype=[('channel', 'S3'), ('value', 'i2')])
 
     ingoing_buffer.put(reading)
     time.sleep(0.1)
@@ -105,7 +102,7 @@ def test_writefiles_short_burst():
                  dtype=[('channel', 'S3'), ('value', 'i2')])
         ingoing_buffer.put(reading)
 
-    time.sleep(1)
+    time.sleep(2)
     assert sc.write_buffer.empty()
     # cleanup
     call(['rm', expected_filename])
