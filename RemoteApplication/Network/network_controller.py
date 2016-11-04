7d3ceb4ed12b1fd04e48cff62447693e91e6db10
pmaclellan <maclellan.pete@gmail.com>
2016-11-04 16:39:38 -0400

    relocated files, pushed sequence # responsibility to GUI, all messages now wrapped and serialized across all boundaries

--- RemoteApplication/Network/network_controller.py
+++ RemoteApplication/Network/network_controller.py
@@ -6,13 +6,25 @@ import sys
 import control_signals_pb2
 
 class ControlClient(asyncore.dispatcher):
-    def __init__(self, host, port, outgoing_queue, sent_dict, incoming_queue):
+    def __init__(self, host, port, outgoing_queue, incoming_queue):
         asyncore.dispatcher.__init__(self)
+
+        # initialize TCP socket
         self.create_socket(socket.AF_INET, socket.SOCK_STREAM)
+
+        # holds messages that have not been sent yet
         self.outgoing_queue = outgoing_queue
-        self.sent_dict = sent_dict
+
+        # holds serialized messages that have been received but not processed
         self.incoming_queue = incoming_queue
+
+        # holds messages that have been sent over the
+        # control_socket but have not yet been ACKed
+        self.sent_dict = {}
+
+        # sequence numbers are assigned to outgoing messages to correlate ACKs
         self.sequence = 0
+
         self.host = host
         self.port = port
         self.connected = False
@@ -30,56 +42,59 @@ class ControlClient(asyncore.dispatcher):
         self.close()
 
     def handle_read(self):
+        # read 16 bit length header
         length = self.recv(2)
         print 'received length header: %s' % length
+
+        # read the message content
         msg = self.recv(length)
         print 'received message: %s' % msg
-        ack = control_signals_pb2.RequestWrapper()
-        ack.ParseFromString(msg)
-        seq = ack.seq
+
+        # TODO: put onto incoming_queue and have another thread handle the parsing
+
+        # construct a container protobuf and parse into from serialized message
+        ackWrapper = control_signals_pb2.RequestWrapper()
+        ackWrapper.ParseFromString(msg)
+
+        seq = ackWrapper.seq
+
         if seq in self.sent_dict.keys():
             acked_request = self.sent_dict.pop(seq)
+            print 'ACKed request popped %s' % acked_request
             # TODO: process ACK and notify necessary parties
 
     def readable(self):
         return True
 
     def writable(self):
+        # we want to write whenever there are messages to be sent
         is_writable = not self.outgoing_queue.empty()
         print 'writable() -> %s' % is_writable
         return is_writable
 
     def handle_write(self):
         # grab request to be sent from the queue
-        request = self.outgoing_queue.get_nowait()
-        print 'retrieved msg type: %s from queue' % type(request)
+        request_wrapper = self.outgoing_queue.get_nowait()
+        print 'retrieved msg type: %s from queue' % type(request_wrapper)
 
-        # wrap request in generic request message
-        wrapped_request = control_signals_pb2.RequestWrapper()
-        wrapped_request.sequence = self.sequence
-        if request is control_signals_pb2._STARTREQUEST:
-            wrapped_request.start = request
-        elif request is control_signals_pb2._STOPREQUEST:
-            wrapped_request.stop = request
-        elif request is control_signals_pb2._SAMPLERATEREQUEST:
-            wrapped_request.rate = request
-        elif request is control_signals_pb2._SENSITIVITYREQUEST:
-            wrapped_request.sens = request
 
-        serialized = wrapped_request.SerializeToString()
+        serialized = request_wrapper.SerializeToString()
+
         print 'sending message length over control socket'
         self.send(str(sys.getsizeof(serialized)))
+        # TODO: sent as a uint16
+
         print 'sending Request message over control socket'
         sent = self.send(serialized)
         print 'sent message bytes: %d' % sent
-        self.sent_dict[self.sequence] = request
+
+        print 'adding request %d to sent_dict' % request_wrapper.sequence
+        self.sent_dict[request_wrapper.sequence] = request_wrapper
         self.sequence += 1
-        print 'next SEQ num: %d' % self.sequence
 
 
 class NetworkController():
     def __init__(self, storage_queue, gui_queue):
-        print 'NetworkController __init__()'
 
         if type(storage_queue) is multiprocessing.queues.Queue:
             self.storage_queue = storage_queue
@@ -88,14 +103,8 @@ class NetworkController():
 
         if type(gui_queue) is multiprocessing.queues.Queue:
             self.gui_queue = gui_queue
-            print 'NC-side: gui_queue received is empty: %s' % gui_queue.empty()
-            print 'NC-side: self.gui_queue is empty: %s' % self.gui_queue.empty()
         else:
             raise TypeError('arg 2 must be a multiprocessing.Queue, found \n%s' % str(type(gui_queue)))
-
-        # sent_dict will hold request messages that have been sent over the
-        # control_socket but have not yet been ACKed
-        self.sent_dict = {}
 
         self.outgoing_queue = multiprocessing.Queue()
         self.received_queue = multiprocessing.Queue()
@@ -103,7 +112,6 @@ class NetworkController():
         # create TCP sockets for communication with DaQuLa
         self.control_client = ControlClient('localhost', 10001,
                                             self.outgoing_queue,
-                                            self.sent_dict,
                                             self.received_queue)
         self.data_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
@@ -123,16 +131,21 @@ class NetworkController():
             self.loop_thread.start()
 
     def recv_from_gui(self):
-        expected_requests = (control_signals_pb2.StartRequest,
-                             control_signals_pb2.StopRequest,
-                             control_signals_pb2.SampleRateRequest,
-                             control_signals_pb2.SensitivityRequest)
         while True:
             if not self.gui_queue.empty() and self.control_client.connected:
-                print 'client connected: %s' % self.control_client.connected
-                request = self.gui_queue.get_nowait()
-                if type(request) not in expected_requests:
-                    raise TypeError('unexpected object in queue from gui, Type: %s' % type(request))
-                else:
-                    print 'putting object in outgoing_queue'
-                    self.outgoing_queue.put(request)
+                requestWrapper = control_signals_pb2.RequestWrapper()
+
+                serialized = self.gui_queue.get_nowait()
+
+                requestWrapper.ParseFromString(serialized)
+                print 'received wrapper %s' % requestWrapper
+
+                # just pass along to control client without modifying
+                self.outgoing_queue.put(requestWrapper)
+
+            elif not self.gui_queue.empty() and not self.control_client.connected:
+                self.gui_queue.get_nowait()
+                print 'NOT CONNECTED: dropped message'
+
+                # TODO: notify GUI that connection to server has not been established
+                # TODO: -> requests cannot be sent at this time
--- RemoteApplication/Network/test_network_controller.py
+++ RemoteApplication/Network/test_network_controller.py
@@ -4,78 +4,111 @@ import control_signals_pb2
 import time
 import socket
 
-# def setup():
-#     stg_queue = mp.Queue()
-#     gui_queue = mp.Queue()
-#     nc = NetworkController(stg_queue, gui_queue)
-#     return (stg_queue, gui_queue, nc)
-
-# def test_creation():
-#     stg_queue, gui_queue, nc = setup()
-#     assert nc.control_client is not None
-#     assert nc.data_sock is not None
-
-# def test_gui_queue_receiver():
-#     stg_queue, gui_queue, nc = setup()
-#     startRequest = control_signals_pb2.StartRequest()
-#     startRequest.port = 0
-#     startRequest.channels = 0xDEADBEEF
-#     gui_queue.put(startRequest)
-#     time.sleep(0.5) # wait for nc to read from queue
-#     assert gui_queue.empty()
-#
-# def test_mult_types_gui_queue_receiver():
-#     stg_queue, gui_queue, nc = setup()
-#     startRequest = control_signals_pb2.StartRequest()
-#     startRequest.port = 0
-#     startRequest.channels = 0xDEADBEEF
-#     stopRequest = control_signals_pb2.StopRequest()
-#     stopRequest.port = 0
-#     stopRequest.channels = 0xDEADBEEF
-#     gui_queue.put(startRequest)
-#     gui_queue.put(stopRequest)
-#     time.sleep(0.5) # wait for nc to read from queue
-#     assert gui_queue.empty()
-
-def test_control_socket_send_request():
-    # stg_queue, gui_queue, nc = setup()
-
+def setup():
     stg_queue = mp.Queue()
     gui_queue = mp.Queue()
     nc = NetworkController(stg_queue, gui_queue)
+    return (stg_queue, gui_queue, nc)
 
-    # simulate message from GUI
+def test_creation():
+    stg_queue, gui_queue, nc = setup()
+    assert nc.control_client is not None
+    assert nc.data_sock is not None
+
+def test_gui_queue_receiver():
+    stg_queue, gui_queue, nc = setup()
+
+    startRequest = control_signals_pb2.StartRequest()
+    startRequest.port = 1
+    startRequest.channels = 0xDEADBEEF
+
+    requestWrapper = control_signals_pb2.RequestWrapper()
+    requestWrapper.sequence = 1
+    requestWrapper.start.MergeFrom(startRequest) # can't assign directly for some reason, need to merge
+
+    gui_queue.put(requestWrapper.SerializeToString())
+
+    time.sleep(0.5) # wait for nc to read from queue
+    assert gui_queue.empty()
+
+def test_mult_types_gui_queue_receiver():
+    stg_queue, gui_queue, nc = setup()
+
+    # construct inner request
     startRequest = control_signals_pb2.StartRequest()
     startRequest.port = 0
     startRequest.channels = 0xDEADBEEF
 
-    # load request into queue to be sent by ControlClient
-    gui_queue.put(startRequest)
+    # construct request wrapper containing inner
+    requestWrapper1 = control_signals_pb2.RequestWrapper()
+    requestWrapper1.sequence = 1
+    requestWrapper1.start.MergeFrom(startRequest)  # can't assign directly for some reason, need to merge
 
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.bind(('localhost', 10001))
-    sock.listen(1)
-    print 'gui-queue empty from test side: %s' % gui_queue.empty()
+    serialized1 = requestWrapper1.SerializeToString()
 
-    nc.connect_control_port()
+    # construct inner request
+    stopRequest = control_signals_pb2.StopRequest()
+    stopRequest.port = 0
+    stopRequest.channels = 0xDEADBEEF
 
-    print 'listening on %s:%d' % ('localhost', 10001)
-    conn, addr = sock.accept()
-    print 'accepted connection from %s:%d' % (addr[0], addr[1])
-    print 'gui-queue empty from test side: %s' % gui_queue.empty()
+    # construct request wrapper containing inner
+    requestWrapper2 = control_signals_pb2.RequestWrapper()
+    requestWrapper2.sequence = 2
+    requestWrapper2.stop.MergeFrom(stopRequest)  # can't assign directly for some reason, need to merge
 
-    length = ''
-    while length == '':
-        length = conn.recv(2)
-        print length
-    print 'test received length header: %s' % length
-    msg = conn.recv(int(length))
-    reply = control_signals_pb2.RequestWrapper()
-    reply.ParseFromString(msg)
-    print 'test received request message:\n%s' % reply
+    serialized2 = requestWrapper2.SerializeToString()
 
-    assert reply.HasField('start')
-    startMessage = reply.start
-    assert startMessage is control_signals_pb2._STARTREQUEST
-    assert startMessage.port == 0
-    assert startMessage.channels == 0xDEADBEEF
+    # pass the serialized message wrappers to the network controller
+    # "from" the GUI
+    gui_queue.put(serialized1)
+    gui_queue.put(serialized2)
+
+    time.sleep(0.5) # wait for nc to read from queue
+    assert gui_queue.empty()
+
+
+# def test_control_socket_send_request():
+#     # stg_queue, gui_queue, nc = setup()
+#
+#     stg_queue = mp.Queue()
+#     gui_queue = mp.Queue()
+#     nc = NetworkController(stg_queue, gui_queue)
+#
+#     print 'trying to create a StartRequest'
+#     # simulate message from GUI
+#     startRequest = control_signals_pb2.StartRequest()
+#     startRequest.port = 0
+#     startRequest.channels = 0xDEADBEEF
+#     print type(startRequest)
+#     print startRequest
+#
+#     # load request into queue to be sent by ControlClient
+#     gui_queue.put(startRequest)
+#
+#     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#     sock.bind(('localhost', 10001))
+#     sock.listen(1)
+#     print 'gui-queue empty from test side: %s' % gui_queue.empty()
+#
+#     nc.connect_control_port()
+#
+#     print 'listening on %s:%d' % ('localhost', 10001)
+#     conn, addr = sock.accept()
+#     print 'accepted connection from %s:%d' % (addr[0], addr[1])
+#     print 'gui-queue empty from test side: %s' % gui_queue.empty()
+#
+#     length = ''
+#     while length == '':
+#         length = conn.recv(2)
+#         print length
+#     print 'test received length header: %s' % length
+#     msg = conn.recv(int(length))
+#     reply = control_signals_pb2.RequestWrapper()
+#     reply.ParseFromString(msg)
+#     print 'test received request message:\n%s' % reply
+#
+#     assert reply.HasField('start')
+#     startMessage = reply.start
+#     assert startMessage is control_signals_pb2._STARTREQUEST
+#     assert startMessage.port == 0
+#     assert startMessage.channels == 0xDEADBEEF
