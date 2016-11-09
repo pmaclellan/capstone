3c559ce5ff0806f2fb2ddbd2a5d1b9695414e6b7
pmaclellan <maclellan.pete@gmail.com>
2016-11-09 13:54:57 -0500

    finished parser stage and added some performance tests

--- RemoteApplication/Network/data_client.py
+++ RemoteApplication/Network/data_client.py
@@ -1,5 +1,6 @@
 import socket
 import multiprocessing as mp
+import numpy as np
 import threading
 import Queue # just needed for the Empty exception
 
@@ -26,7 +27,7 @@ class DataClient():
 
         self.recv_stop_event = threading.Event()
         self.receiver_thread = threading.Thread(target=self.receive_data, args=[self.recv_stop_event])
-        self.sync_recovery_thread = threading.Thread(target=self.syncronize_stream, args=[self.recv_stop_event])
+        self.sync_recovery_thread = threading.Thread(target=self.synchronize_stream, args=[self.recv_stop_event])
         self.parser_thread = threading.Thread(target=self.parse_readings, args=[self.recv_stop_event])
 
         self.host = host
@@ -36,7 +37,6 @@ class DataClient():
 
     def start_receiver_thread(self):
         self.receiver_thread.start()
-
     def stop_data_threads(self):
         self.recv_stop_event.set()
 
@@ -48,10 +48,10 @@ class DataClient():
 
     def update_active_channels(self, active_channels):
         self.active_channels = active_channels
-        print 'ACTIVE CHANNELS: %s' % self.active_channels
+        # print 'ACTIVE CHANNELS: %s' % self.active_channels
 
     def connect_data_port(self):
-        print 'DataClient: attempting connection'
+        # print 'DataClient: attempting connection'
         self.sock.connect((self.host, self.port))
         self.start_receiver_thread()
         self.start_sync_recovery_thread()
@@ -59,7 +59,7 @@ class DataClient():
         self.connected = True
 
     def close_data_port(self):
-        print 'DataClient: close_control_port()'
+        # print 'DataClient: close_control_port()'
         self.connected = False
         self.stop_data_threads()
         self.sock.close()
@@ -71,8 +71,8 @@ class DataClient():
             if byte != '':
                 self.incoming_queue.put(byte)
 
-    def syncronize_stream(self, *args):
-        print '\n\nsync thread started'
+    def synchronize_stream(self, *args):
+        # print '\n\nsync thread started'
         stop_event = args[0]
         self.synchronized = False
         in_timestamp = False
@@ -88,7 +88,7 @@ class DataClient():
 
             # special logic to grab 48-bit timestamp
             if in_timestamp:
-                print 'gathering timestamp bytes'
+                # print 'gathering timestamp bytes'
                 ts_bytes = []
                 for i in range(6):
                     ts_byte = self.incoming_queue.get_nowait()
@@ -96,7 +96,7 @@ class DataClient():
                 ts_value = int(ts_bytes[5] + ts_bytes[4] + ts_bytes[3] +
                                ts_bytes[2] + ts_bytes[1] + ts_bytes[0], 16)
                 temp_buffer.append(ts_value)
-                print 'added timestamp, new buffer is %s' % temp_buffer
+                # print 'added timestamp, new buffer is %s' % temp_buffer
                 in_timestamp = False
 
             # realign in case we got a stray byte somewhere and are off by one byte
@@ -113,29 +113,29 @@ class DataClient():
                     byte1 = self.incoming_queue.get_nowait()
                     byte1_enc = byte1.encode('hex')
                 except Queue.Empty:
-                    print 'incoming_queue empty, skipping to next iteration'
+                    # print 'incoming_queue empty, skipping to next iteration'
                     continue
 
             # merge the two byte strings and convert to an integer
             value = int(byte1_enc + byte2_enc, 16)
-            # print 'bytes: %s, %s' % (byte1_enc, byte2_enc)
+#             # print 'bytes: %s, %s' % (byte1_enc, byte2_enc)
 
             if not self.synchronized:
                 if byte1_enc == 'de':
                     if byte2_enc == 'ad':
                         # we found a DEAD, let's see if it aligns with a previous DEAD
-                        print 'DEAD found, temp_buffer length is %d' % len(temp_buffer)
+                        # print 'DEAD found, temp_buffer length is %d' % len(temp_buffer)
                         # we need to use '+ 4' here because the timestamp hasn't been recognized yet
                         if len(temp_buffer) >= (n + 4) and temp_buffer[-1 * (n + 4)] == 57005:  # 0xDEAD
                             self.synchronized = True
-                            print 'second DEAD reached, syncronized = True'
+                            # print 'second DEAD reached, syncronized = True'
                             # flush the buffer to only include the new DEAD
                             temp_buffer = [value]
                             in_timestamp = True
                             continue
                         else:
                             # not in sync yet, store in buffer
-                            print 'no previous DEAD found, continuing...'
+                            # print 'no previous DEAD found, continuing...'
                             temp_buffer.append(value)
                             continue
                     else:
@@ -145,7 +145,7 @@ class DataClient():
                 elif byte2_enc == 'de':
                     if temp_byte == 'ad': # where temp_byte is the byte1_enc from the prev iteration
                         # we found a DEAD, but we're misaligned by one byte
-                        print 'misaligned DEAD detected, attempting recovery'
+                        # print 'misaligned DEAD detected, attempting recovery'
                         # reconstruct binary string with the DEAD together
                         value = int(byte2_enc + temp_byte, base=16)
                         temp_buffer = [value]
@@ -166,12 +166,12 @@ class DataClient():
 
             elif self.synchronized:
                 if value == 57005:  # 0xDEAD
-                    print 'found a DEAD while synced'
+                    # print 'found a DEAD while synced'
                     # the '+ 2' accounts for the DEAD and timestamp
                     if len(temp_buffer) >= (n + 2) and temp_buffer[-1 * (n + 2)] == 57005:
                         # all good, still synced up
                         # push the most recent set of readings to the parsing stage
-                        print 'S&R: passing buffer to pipeline_queue'
+                        # print 'S&R: passing buffer to pipeline_queue'
                         self.pipeline_queue.put(temp_buffer)
                         temp_buffer = [value]  # flush the buffer
                         # grab the timestamp next time around
@@ -184,7 +184,7 @@ class DataClient():
                         continue
                 elif byte2_enc == 'de' and temp_byte == 'ad':
                     # either we got really unlucky, or we've fallen out of sync again
-                    print 'split DEAD found, syncronized -> False'
+                    # print 'split DEAD found, syncronized -> False'
                     value = int(byte2_enc + temp_byte, base=16)
                     temp_buffer = [value]
                     realign_by_one = True
@@ -197,6 +197,8 @@ class DataClient():
                     temp_byte = byte1_enc
                     continue
 
+    # Parser is in charge of transforming the list output from the Sync and Recovery
+    # stage into a NumPy array for use in the GUI and StorageController
     def parse_readings(self, *args):
         stop_event = args[0]
         while not stop_event.is_set():
@@ -205,5 +207,23 @@ class DataClient():
             except Queue.Empty:
                 continue
 
-            print buffer
-            self.parser_working = True
+            # make sure we have the proper number of data points
+            assert len(buffer) == len(self.active_channels) + 2
+
+            # start by grabbing the timestamp and putting it in a temp list
+            intermediate = [('_TS', buffer[1])]
+
+            # trim DEAD and timestamp from the buffer
+            buffer = buffer[2:]
+
+            # construct tuple for each reading e.g. ('0.0', 43125) and add to list
+            for i in range(len(buffer)):
+                intermediate.append((self.active_channels[i], buffer[i]))
+
+            # create numpy array from list for passing to storage
+            reading = np.array(intermediate, dtype=[('channel', 'S3'), ('value', 'uint64')])
+
+            # pass the reading along to the other components
+            self.gui_queue.put(reading)
+            self.storage_queue.put(reading)
+
--- RemoteApplication/Network/test_data_client.py
+++ RemoteApplication/Network/test_data_client.py
@@ -1,10 +1,7 @@
 from data_client import DataClient
 
-import pytest
 import multiprocessing as mp
 import time
-import socket
-import sys
 
 host = 'foo'
 port = 5
@@ -32,6 +29,16 @@ dead_sequence = ['\xad', '\xde', '\xaa', '\xaa', '\xaa', '\xaa', '\xaa', '\xaa',
                  '\xff', '\xee', '\xff', '\xee', '\xff', '\xee', '\xff', '\xee',
                  '\xff', '\xee', '\xff', '\xee', '\xff', '\xee', '\xff', '\xee',
                  '\xff', '\xee', '\xff', '\xee', '\xff', '\xee', '\xff', '\xee']
+
+parser_input = [57005, 187649984473770,
+                61183, 61183, 61183, 61183,
+                61183, 61183, 61183, 61183,
+                61183, 61183, 61183, 61183,
+                61183, 61183, 61183, 61183,
+                61183, 61183, 61183, 61183,
+                61183, 61183, 61183, 61183,
+                61183, 61183, 61183, 61183,
+                61183, 61183, 61183, 61183]
 
 active_channels = ['0.0', '0.1', '0.2', '0.3', '0.4', '0.5', '0.6', '0.7',
                    '1.0', '1.1', '1.2', '1.3', '1.4', '1.5', '1.6', '1.7',
@@ -149,4 +156,51 @@ class TestCombinedStages:
                 dc.incoming_queue.put(x)
 
         time.sleep(0.2)
-        assert dc.parser_working
+        assert not dc.storage_queue.empty()
+        assert not dc.gui_queue.empty()
+
+class TestPerformance:
+    def test_sync_recovery_speed(self):
+        dc = DataClient(host, port, storage_queue, gui_data_queue, active_channels)
+
+        dc.start_sync_recovery_thread()
+
+        input_length = 1000
+
+        start = time.time()
+
+        for i in range(input_length):
+            for x in normal_sequence:
+                dc.incoming_queue.put(x)
+
+        while not dc.incoming_queue.empty():
+            # wait for the parser to finish processing the input
+            continue
+
+        elapsed = time.time() - start
+
+        speed = 1 / (elapsed / input_length)
+
+        print '\n\nSync Recovery Stage: effective frequency over %d samples is %d Hz\n' % (input_length, speed)
+
+    def test_parser_speed(self):
+        dc = DataClient(host, port, storage_queue, gui_data_queue, active_channels)
+
+        dc.start_parser_thread()
+
+        input_length = 10000
+
+        start = time.time()
+
+        for i in range(input_length):
+            dc.pipeline_queue.put(parser_input)
+
+        while not dc.pipeline_queue.empty():
+            # wait for the parser to finish processing the input
+            continue
+
+        elapsed = time.time() - start
+
+        speed = 1 / (elapsed / input_length)
+
+        print '\n\nParser Stage: effective frequency over %d samples is %d Hz\n' % (input_length, speed)
