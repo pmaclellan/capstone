3c575bce43a55517cb85a89041b29912bcf0b7d5
pmaclellan <maclellan.pete@gmail.com>
2016-11-13 10:58:22 -0500

    inherit from mp.Process and use Pipe instead of Queue

--- RemoteApplication/Network/network_controller.py
+++ RemoteApplication/Network/network_controller.py
@@ -8,37 +8,35 @@ from control_client import ControlClient
 from data_client import DataClient
 
 
-class NetworkController():
-    def __init__(self, storage_queue, from_gui_queue, to_gui_queue, gui_data_queue):
+class NetworkController(mp.Process):
+    def __init__(self, storage_sender, gui_control_conn, gui_data_sender,
+                 reading_to_be_stored_cond, readings_to_be_plotted_cond):
+        super(NetworkController, self).__init__()
 
-        if type(storage_queue) is mp.queues.Queue:
-            self.storage_queue = storage_queue
-        else:
-            raise TypeError('arg 1 must be a multiprocessing.Queue, found \n%s' % str(type(storage_queue)))
+        # mp.Connection for sending readings from DataClient to StorageController
+        self.storage_sender = storage_sender
 
-        if type(from_gui_queue) is mp.queues.Queue:
-            self.from_gui_queue = from_gui_queue
-        else:
-            raise TypeError('arg 2 must be a multiprocessing.Queue, found \n%s' % str(type(from_gui_queue)))
+        # mp.Connection for sending  and receiving control messages (protobufs) back and forth to GUI
+        # Note: full duplex Pipe
+        self.gui_control_conn = gui_control_conn
 
-        if type(to_gui_queue) is mp.queues.Queue:
-            self.to_gui_queue = to_gui_queue
-        else:
-            raise TypeError('arg 2 must be a multiprocessing.Queue, found \n%s' % str(type(to_gui_queue)))
+        # mp.Connection for sending ADC readings to GUI for plotting
+        self.gui_data_sender = gui_data_sender
 
-        if type(gui_data_queue) is mp.queues.Queue:
-            self.gui_data_queue = gui_data_queue
-        else:
-            raise TypeError('arg 3 must be a multiprocessing.Queue, found \n%s' % str(type(gui_data_queue)))
+        # IPC condition variables
+        self.reading_to_be_stored_cond = reading_to_be_stored_cond
+        self.readings_to_be_plotted_cond = readings_to_be_plotted_cond
 
-        # shared with control client, holds request messages to be sent
-        self.outgoing_queue = mp.Queue()
+        # shared with control client, sends request messages to be sent over TCP
+        # receives ACK messages
+        self.nc_control_conn, self.cc_control_conn = mp.Pipe(duplex=True)
 
         # control client will write ACK'd requests here
         self.ack_queue = mp.Queue()
 
         # default to all channels being active
-        # NOTE: this needs to match up with the default state of the channel checboxes on GUI
+        # NOTE: this needs to match up with the default state of the channel checkboxes on GUI
+        # and needs to be propagated to DataClient upon any change
         self.active_channels = ['0.0', '0.1', '0.2', '0.3', '0.4', '0.5', '0.6', '0.7',
                                 '1.0', '1.1', '1.2', '1.3', '1.4', '1.5', '1.6', '1.7',
                                 '2.0', '2.1', '2.2', '2.3', '2.4', '2.5', '2.6', '2.7',
@@ -46,23 +44,24 @@ class NetworkController():
 
         self.channel_mask = self.generateChannelBitMask()
 
-        # create TCP sockets for communication with DaQuLa
-        self.control_client = ControlClient('localhost', 10001,
-                                            self.outgoing_queue,
-                                            self.ack_queue)
-        self.data_client = DataClient('localhost', 10002,
-                                      self.gui_data_queue,
-                                      self.storage_queue,
-                                      self.active_channels)
+        self.control_client = ControlClient(host='localhost',
+                                            port=10001,
+                                            control_protobuf_conn=self.cc_control_conn)
+
+        self.data_client = DataClient(host='localhost',
+                                      port=10002,
+                                      gui_data_sender=self.gui_data_sender,
+                                      storage_sender=self.storage_sender,
+                                      reading_to_be_stored_cond=self.reading_to_be_stored_cond,
+                                      readings_to_be_plotted_cond=self.readings_to_be_plotted_cond)
 
         # receives request protobuf messages triggered by GUI events
         self.gui_receiver_thread = threading.Thread(target=self.recv_from_gui)
         self.gui_receiver_thread.daemon = True
-        self.gui_receiver_thread.start()
 
         # listens for ACK messages being passed back from control client
         self.ack_listener_thread = threading.Thread(target=self.read_ack_messages)
-        self.ack_listener_thread.start()
+        self.ack_listener_thread.daemon = True
 
         # handle asyncore blocking loop in a separate thread
         # NOTE: lambda needed so loop() doesn't get called right away and block
@@ -70,6 +69,11 @@ class NetworkController():
         # use_poll=True is a workaround to avoid "bad file descriptor" upon closing
         # for python 2.7.X according to GitHub Issue...but it still gives the error
         self.loop_thread = threading.Thread(target=lambda: asyncore.loop(1.0, use_poll=True))
+
+    def run(self):
+        self.gui_receiver_thread.start()
+        self.ack_listener_thread.start()
+        self.gui_receiver_thread.join()
 
     def connect_control_port(self):
         print 'connect_control_port() entered'
@@ -89,7 +93,7 @@ class NetworkController():
         self.loop_thread.join()
 
     def close_data_port(self):
-        self.control_client.close_data_port()
+        self.data_client.close_data_port()
 
     def processChannelUpdate(self, sender, checked):
         if checked:
@@ -120,10 +124,10 @@ class NetworkController():
 
     def recv_from_gui(self):
         while True:
-            if not self.from_gui_queue.empty() and self.control_client.connected:
+            if self.gui_control_conn.poll() and self.control_client.connected:
                 requestWrapper = control_signals_pb2.RequestWrapper()
 
-                serialized = self.from_gui_queue.get_nowait()
+                serialized = self.gui_control_conn.recv()
 
                 requestWrapper.ParseFromString(serialized)
                 print 'received wrapper %s' % requestWrapper
@@ -131,8 +135,8 @@ class NetworkController():
                 # just pass along to control client without modifying
                 self.outgoing_queue.put(serialized)
 
-            elif not self.from_gui_queue.empty() and not self.control_client.connected:
-                self.from_gui_queue.get_nowait()
+            elif self.gui_control_conn.poll() and not self.control_client.connected:
+                self.gui_control_conn.recv()
                 print 'NOT CONNECTED: dropped message'
 
                 # TODO: notify GUI that connection to server has not been established
@@ -150,12 +154,12 @@ class NetworkController():
                     print 'NetworkController: received start ACK, starting data client'
                     try:
                         self.data_client.connect_data_port()
-                        self.to_gui_queue.put(acked)
+                        self.gui_control_conn.send(acked)
                     except Exception, e:
                         print('ERROR: could not connect data client, exception is %s' % e)
 
                 elif self.data_client.connected:
                     # passthrough
-                    self.to_gui_queue.put(acked)
+                    self.gui_control_conn.send(acked)
 
 
