26bfe88599b8b1d82a86ed0802bf0ad80efa4731
pmaclellan <maclellan.pete@gmail.com>
2016-11-15 15:20:22 -0500

    added timestamp field to protobuf StartRequest

--- RemoteApplication/control_signals_pb2.py
+++ RemoteApplication/control_signals_pb2.py
@@ -19,7 +19,7 @@ DESCRIPTOR = _descriptor.FileDescriptor(
   name='control_signals.proto',
   package='',
   syntax='proto3',
-  serialized_pb=_b('\n\x15\x63ontrol_signals.proto\"<\n\x0cStartRequest\x12\x0c\n\x04port\x18\x01 \x01(\r\x12\x10\n\x08\x63hannels\x18\x02 \x01(\r\x12\x0c\n\x04rate\x18\x03 \x01(\r\"-\n\x0bStopRequest\x12\x0c\n\x04port\x18\x01 \x01(\r\x12\x10\n\x08\x63hannels\x18\x02 \x01(\r\"j\n\x12SensitivityRequest\x12,\n\x07voltage\x18\x01 \x01(\x0e\x32\x1b.SensitivityRequest.Voltage\"&\n\x07Voltage\x12\x08\n\x04\x46IVE\x10\x00\x12\x07\n\x03TEN\x10\x01\x12\x08\n\x04VREF\x10\x02\"\x8c\x01\n\x0eRequestWrapper\x12\x10\n\x08sequence\x18\x01 \x01(\r\x12\x1e\n\x05start\x18\x02 \x01(\x0b\x32\r.StartRequestH\x00\x12\x1c\n\x04stop\x18\x03 \x01(\x0b\x32\x0c.StopRequestH\x00\x12#\n\x04sens\x18\x04 \x01(\x0b\x32\x13.SensitivityRequestH\x00\x42\x05\n\x03msgb\x06proto3')
+  serialized_pb=_b('\n\x15\x63ontrol_signals.proto\"O\n\x0cStartRequest\x12\x0c\n\x04port\x18\x01 \x01(\r\x12\x10\n\x08\x63hannels\x18\x02 \x01(\r\x12\x0c\n\x04rate\x18\x03 \x01(\r\x12\x11\n\ttimestamp\x18\x04 \x01(\x04\"-\n\x0bStopRequest\x12\x0c\n\x04port\x18\x01 \x01(\r\x12\x10\n\x08\x63hannels\x18\x02 \x01(\r\"j\n\x12SensitivityRequest\x12,\n\x07voltage\x18\x01 \x01(\x0e\x32\x1b.SensitivityRequest.Voltage\"&\n\x07Voltage\x12\x08\n\x04\x46IVE\x10\x00\x12\x07\n\x03TEN\x10\x01\x12\x08\n\x04VREF\x10\x02\"\x8c\x01\n\x0eRequestWrapper\x12\x10\n\x08sequence\x18\x01 \x01(\r\x12\x1e\n\x05start\x18\x02 \x01(\x0b\x32\r.StartRequestH\x00\x12\x1c\n\x04stop\x18\x03 \x01(\x0b\x32\x0c.StopRequestH\x00\x12#\n\x04sens\x18\x04 \x01(\x0b\x32\x13.SensitivityRequestH\x00\x42\x05\n\x03msgb\x06proto3')
 )
 _sym_db.RegisterFileDescriptor(DESCRIPTOR)
 
@@ -46,8 +46,8 @@ _SENSITIVITYREQUEST_VOLTAGE = _descriptor.EnumDescriptor(
   ],
   containing_type=None,
   options=None,
-  serialized_start=202,
-  serialized_end=240,
+  serialized_start=221,
+  serialized_end=259,
 )
 _sym_db.RegisterEnumDescriptor(_SENSITIVITYREQUEST_VOLTAGE)
 
@@ -80,6 +80,13 @@ _STARTREQUEST = _descriptor.Descriptor(
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
       options=None),
+    _descriptor.FieldDescriptor(
+      name='timestamp', full_name='StartRequest.timestamp', index=3,
+      number=4, type=4, cpp_type=4, label=1,
+      has_default_value=False, default_value=0,
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      options=None),
   ],
   extensions=[
   ],
@@ -93,7 +100,7 @@ _STARTREQUEST = _descriptor.Descriptor(
   oneofs=[
   ],
   serialized_start=25,
-  serialized_end=85,
+  serialized_end=104,
 )
 
 
@@ -130,8 +137,8 @@ _STOPREQUEST = _descriptor.Descriptor(
   extension_ranges=[],
   oneofs=[
   ],
-  serialized_start=87,
-  serialized_end=132,
+  serialized_start=106,
+  serialized_end=151,
 )
 
 
@@ -162,8 +169,8 @@ _SENSITIVITYREQUEST = _descriptor.Descriptor(
   extension_ranges=[],
   oneofs=[
   ],
-  serialized_start=134,
-  serialized_end=240,
+  serialized_start=153,
+  serialized_end=259,
 )
 
 
@@ -217,8 +224,8 @@ _REQUESTWRAPPER = _descriptor.Descriptor(
       name='msg', full_name='RequestWrapper.msg',
       index=0, containing_type=None, fields=[]),
   ],
-  serialized_start=243,
-  serialized_end=383,
+  serialized_start=262,
+  serialized_end=402,
 )
 
 _SENSITIVITYREQUEST.fields_by_name['voltage'].enum_type = _SENSITIVITYREQUEST_VOLTAGE
