ab8054660a619d9eefb37af3c1691692aa3f9969
pmaclellan <maclellan.pete@gmail.com>
2016-11-15 16:03:04 -0500

    more IPC stuff

--- RemoteApplication/daqula.py
+++ RemoteApplication/daqula.py
@@ -23,13 +23,18 @@ class DaqulaApplication(mp.Process):
         self.gui_data_receiver, self.gui_data_sender = mp.Pipe(duplex=False)
         # GUI -> SC
         self.filepath_receiver, self.filepath_sender = mp.Pipe(duplex=False)
+        # NC -> SC
+        self.file_header_receiver, self.file_header_sender = mp.Pipe(duplex=False)
 
         # Create Condition variables
-        self.reading_to_be_stored_cond = mp.Condition()
         self.readings_to_be_plotted_cond = mp.Condition()
         self.filepath_available_cond = mp.Condition()
+        self.file_header_available_cond = mp.Condition()
         self.control_msg_from_gui_cond = mp.Condition()
         self.control_msg_from_nc_cond = mp.Condition()
+
+        # Create Event variables for things that need timeout functionality
+        self.reading_to_be_stored_event = mp.Event()
 
         self.gui = MainWindow(control_conn=self.gui_control_conn,
                               data_receiver=self.gui_data_receiver,
@@ -42,15 +47,19 @@ class DaqulaApplication(mp.Process):
         self.nc = NetworkController(storage_sender=self.storage_sender,
                                     gui_control_conn=self.nc_control_conn,
                                     gui_data_sender=self.gui_data_sender,
-                                    reading_to_be_stored_cond=self.reading_to_be_stored_cond,
+                                    file_header_sender=self.file_header_sender,
+                                    file_header_available_cond=self.file_header_available_cond,
+                                    reading_to_be_stored_event=self.reading_to_be_stored_event,
                                     readings_to_be_plotted_cond=self.readings_to_be_plotted_cond,
                                     control_msg_from_gui_cond=self.control_msg_from_gui_cond,
                                     control_msg_from_nc_cond=self.control_msg_from_nc_cond)
 
         self.sc = StorageController(storage_receiver=self.storage_receiver,
                                     filepath_receiver=self.filepath_receiver,
-                                    reading_to_be_stored_cond=self.reading_to_be_stored_cond,
-                                    filepath_available_cond=self.filepath_available_cond)
+                                    file_header_receiver=self.file_header_receiver,
+                                    reading_to_be_stored_event=self.reading_to_be_stored_event,
+                                    filepath_available_cond=self.filepath_available_cond,
+                                    file_header_available_cond=self.file_header_available_cond)
 
         self.nc.start()
         self.sc.start()
