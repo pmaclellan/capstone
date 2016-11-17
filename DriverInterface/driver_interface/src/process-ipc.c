681a3837fd56863769f71a5f79dde0742fed23a6
Dominic Harkness <dominic.harkness@gmail.com>
2016-11-17 09:53:29 -0500

    Accidentally did the wrong formatting in last commit. Fixed.

--- DriverInterface/driver_interface/src/process-ipc.c
+++ DriverInterface/driver_interface/src/process-ipc.c
@@ -21,109 +21,119 @@
 #define MAX_CONNECTIONS 5
 #define SOCK_PATH "/tmp/controller.sock"
 
-int setupConnection() {
-	int serverSock;
-	int clientSock;
-	int len;
-	socklen_t t;
-	struct sockaddr_un local;
-	struct sockaddr_un remote;
+int setupConnection()
+{
+    int serverSock;
+    int clientSock;
+    int len;
+    socklen_t t;
+    struct sockaddr_un local;
+    struct sockaddr_un remote;
 
-	// Create the socket
-	if ((serverSock = socket(AF_UNIX, SOCK_STREAM, 0)) == -1) {
-		perror("Error creating controller socket");
-		exit(1);
-	}
+    // Create the socket
+    if((serverSock = socket(AF_UNIX, SOCK_STREAM, 0)) == -1)
+    {
+        perror("Error creating controller socket");
+        exit(1);
+    }
 
-	// Setup socket path
-	local.sun_family = AF_UNIX;
-	strcpy(local.sun_path, SOCK_PATH);
-	unlink(local.sun_path);
-	len = strlen(local.sun_path) + sizeof(local.sun_family);
+    // Setup socket path
+    local.sun_family = AF_UNIX;
+    strcpy(local.sun_path, SOCK_PATH);
+    unlink(local.sun_path);
+    len = strlen(local.sun_path) + sizeof(local.sun_family);
 
-	// Bind the socket
-	if (bind(serverSock, (struct sockaddr *) &local, len) == -1) {
-		perror("Error binding controller socket");
-		exit(1);
-	}
+    // Bind the socket
+    if(bind(serverSock, (struct sockaddr *) &local, len) == -1)
+    {
+        perror("Error binding controller socket");
+        exit(1);
+    }
 
-	// Listen for connections
-	if (listen(serverSock, MAX_CONNECTIONS) == -1) {
-		perror("Error listening for socket connections");
-		exit(1);
-	}
+    // Listen for connections
+    if(listen(serverSock, MAX_CONNECTIONS) == -1)
+    {
+        perror("Error listening for socket connections");
+        exit(1);
+    }
 
-	printf("Waiting for a connection...\n");
-	t = (socklen_t) sizeof(remote);
-	if ((clientSock = accept(serverSock, (struct sockaddr *) &remote, &t))
-			== -1) {
-		perror("Error accepting socket connection");
-		exit(1);
-	}
+    printf("Waiting for a connection...\n");
+    t = (socklen_t) sizeof(remote);
+    if((clientSock = accept(serverSock, (struct sockaddr *) &remote, &t)) == -1)
+    {
+        perror("Error accepting socket connection");
+        exit(1);
+    }
 
-	printf("Connected.\n");
-	return clientSock;
+    printf("Connected.\n");
+    return clientSock;
 }
 
-uint64_t readFromServer(int clientSocket) {
-	int size;
-	uint64_t retValue;
+uint64_t readFromServer(int clientSocket)
+{
+    int size;
+    uint64_t retValue;
 
-	size = recv(clientSocket, &retValue, sizeof(uint64_t), 0);
+    size = recv(clientSocket, &retValue, sizeof(uint64_t), 0);
 
-	if (size != sizeof(uint64_t)) {
-		perror("Error reading data from server.\n");
-		exit(1);
-	}
+    if(size != sizeof(uint64_t))
+    {
+        perror("Error reading data from server.\n");
+        exit(1);
+    }
 
-	printf("Received 0x%" PRIx64 "\n", retValue);
+    printf("Received 0x%" PRIx64 "\n", retValue);
 
-	return retValue;
+    return retValue;
 }
 
-void sendToServer(int clientSocket, uint64_t value) {
-	int size;
+void sendToServer(int clientSocket, uint64_t value)
+{
+    int size;
 
-	printf("Sending 0x%" PRIX64 "\n", value);
-	size = send(clientSocket, &value, sizeof(uint64_t), 0);
-	if (size != sizeof(uint64_t)) {
-		perror("Error sending data to server.\n");
-		exit(1);
-	}
+    printf("Sending 0x%" PRIX64 "\n", value);
+    size = send(clientSocket, &value, sizeof(uint64_t), 0);
+    if(size != sizeof(uint64_t))
+    {
+        perror("Error sending data to server.\n");
+        exit(1);
+    }
 }
 
-uint64_t processStartRequest(uint64_t buf) {
-	uint64_t retValue;
-	struct timespec currentTime;
+uint64_t processStartRequest(uint64_t buf)
+{
+    uint64_t retValue;
+    struct timespec currentTime;
 
-	// Get the current time
-	clock_gettime(CLOCK_REALTIME, &currentTime);
+    // Get the current time
+    clock_gettime(CLOCK_REALTIME, &currentTime);
 
-	// Convert time to a uint64_t
-	retValue = BILLION * (uint64_t) (currentTime.tv_sec)
-			+ (uint64_t) (currentTime.tv_nsec);
+    // Convert time to a uint64_t
+    retValue = BILLION * (uint64_t) (currentTime.tv_sec)
+            + (uint64_t) (currentTime.tv_nsec);
 
-	// Get the sample frequency from the request
-	uint32_t sampleFreq = (uint32_t) (buf & 0xFFFFFFFF);
+    // Get the sample frequency from the request
+    uint32_t sampleFreq = (uint32_t) (buf & 0xFFFFFFFF);
 
-	// Sample frequency is in Hz... convert according to:
-	// configValue = 100MHz / sampleFreq
-	uint16_t configValue = (uint16_t) (DMA_TIMER_FREQ / sampleFreq);
+    // Sample frequency is in Hz... convert according to:
+    // configValue = 100MHz / sampleFreq
+    uint16_t configValue = (uint16_t) (DMA_TIMER_FREQ / sampleFreq);
 
-	// Start the DMA
-	printf("Configuring dma with 0x%04x\n", configValue);
-	startDMA(configValue);
+    // Start the DMA
+    printf("Configuring dma with 0x%04x\n", configValue);
+    startDMA(configValue);
 
-	// TODO: Await started status from DMA
+    // TODO: Await started status from DMA
 
-	// Return the time
-	return retValue;
+    // Return the time
+    return retValue;
 }
 
-void processStopRequest() {
-	// Stop the DMA
-	stopDMA();
+void processStopRequest()
+{
+    // Stop the DMA
+    stopDMA();
 
-	// TODO: Await stopped status from DMA
+    // TODO: Await stopped status from DMA
 }
 
