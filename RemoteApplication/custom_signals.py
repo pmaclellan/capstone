a7c16846ebfe93ff460ca71699c597ca54bf5ef7
pmaclellan <maclellan.pete@gmail.com>
2016-11-20 16:33:23 -0500

    added headless mode, still doing some bug fixing

--- RemoteApplication/custom_signals.py
+++
@@ -1,7 +0,0 @@
-from PyQt4 import QtCore
-
-class CustomSignal(QtCore.QObject):
-
-    connected = QtCore.pyqtSignal()
-    disconnected = QtCore.pyqtSignal()
-    new_data = QtCore.pyqtSignal(dict)
